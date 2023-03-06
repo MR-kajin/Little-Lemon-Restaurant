1426f1a2815ed1c0f984266eb6950d7df734f3a9
Kajin-MSI <marco.raffaele@hotmail.com>
2023-03-06 16:07:57 +0100

    added meta tags

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from "./logo.png"
+
+function Header(){
+
+return (
+        <header className='.App-header'>
+            <img src={logo} width={300}  alt='logo'/>
+        </header>
+)
+}
+
+export default Header;
