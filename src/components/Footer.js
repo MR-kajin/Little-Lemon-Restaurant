e9d833e386e5db2a38dc576f71bab847e4e456df
Kajin-MSI <marco.raffaele@hotmail.com>
2023-03-14 11:08:01 +0100

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import { Link } from "react-router-dom"
 
-function Footer() {
+export default function Footer() {
+    return (
+        <footer>
+            <menu>
+                <li className="img-footer">
+                    <img
+                        src={require("../assets/footer-logo.png")}
+                        alt="Little Lemon logo"
+                    />
+                </li>
 
-return (
-    <footer></footer>
-)
+                <li className="contact">
+                    <h1 className="footer-header">Navigation</h1>
+                    <ul className="footer-links">
+                        <Link to="/home">Home</Link>
+                        <Link to="/about">About</Link>
+                        <a
+                            href={require("../assets/menu.webp")}
+                            target="_blank"
+                            rel="noreferrer"
+                        >Menu
+                        </a>
+                        <Link to="/reservation">Reservations</Link>
+                        <Link to="/order">Order</Link>
+                        <Link to="/login">Login</Link>
+                    </ul>
+                </li>
+
+                <li>
+                    <h1 className="footer-header">Contacts</h1>
+                    <ul className="footer-links">
+                        <li>2395 Maldove Way,</li>
+                        <li>Chicago Illinoi</li>
+                        <br></br>
+                        <li>(629)-243-6827</li>
+                        <a
+                            href="mailto: info@littlelemon.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            info@littlelemon.com
+                        </a>
+                    </ul>
+                </li>
+
+                <li>
+                    <h1 className="footer-header">Connect</h1>
+                    <ul className="footer-links">
+                        <a
+                            href="https://www.facebook.com/thelittlelemonshop/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Facebook
+                        </a>
+                        <a
+                            href="https://www.instagram.com/littlelemonmoon/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Instagram
+                        </a>
+                        <a
+                            href="https://www.youtube.com/watch?v=3Li-FfypZYE"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Join us!
+                        </a>
+                    </ul>
+                </li>
+            </menu>
+        </footer>
+    )
 }
-export default Footer;
